6e75c3df11f0b70310eb0b934997207520d064c3
Zubair Farooqui <zubairfarooqui.here@gmail.com>
2024-03-14 21:57:04 +0530

    add userName in response

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,13 +5,6 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   const error = new Error("Validation failed.");
-  //   error.statusCode = 422;
-  //   error.data = errors.array();
-  //   throw error;
-  // }
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
@@ -72,6 +65,7 @@ exports.login = async (req, res, next) => {
     res.status(200).json({
       token: token,
       userId: loadedUser._id.toString(),
+      userName: loadedUser.name,
       expiresIn: 1,
     });
   } catch (err) {
